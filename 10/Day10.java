551abca0188756cb3e313a229a2e251cdc8ddad7
Henning Morud <henning.morud@xlent.no>
2024-12-02 15:56:37 +0100

    Add framework for day 10 (Java)

---
+++ 10/Day10.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Day10
+{
+    public static void main(String []args)
+    {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNext()) {
+            System.out.println(input.nextLine());
+        }
+    }
+};
