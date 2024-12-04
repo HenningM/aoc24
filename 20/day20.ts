4735209246bb7e88df88aa6726c6babc48ea1ffb
Henning Morud <henning.morud@xlent.no>
2024-12-04 18:06:18 +0100

    Add framework for day 20 (TypeScript)

---
+++ 20/day20.ts
@@ -0,0 +1,15 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false
+});
+
+rl.on('line', (line: String) => {
+    console.log(line);
+});
+
+rl.once('close', () => {
+     // end of input
+ });
